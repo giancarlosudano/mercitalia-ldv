aa6601a7353024e61a1a0b49b906384625c0fcc0
Giancarlo Sudano <gisudano@microsoft.com>
2024-10-28 05:44:18 +0100

    fix auth

--- code/admin/Home.py
+++ code/admin/Home.py
@@ -4,7 +4,6 @@ import os
 import logging
 from dotenv import load_dotenv
 import streamlit_authenticator as stauth
-import xml.etree.ElementTree as ET
 
 load_dotenv()
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -31,47 +30,11 @@ authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
+    config['cookie']['expiry_days']
 )
+authenticator.logout(location='sidebar')
 
-name, authentication_status, username = authenticator.login(location='main')
+authenticator.login(location='main')
 
-if username == 'smith@mercitalia.com':
-    st.session_state["authentication_status"] = True
-    st.session_state["name"] = "John Smith"
-
-# # # begin test
-# # from azure.core.credentials import AzureKeyCredential
-# # from azure.ai.formrecognizer import DocumentAnalysisClient
-
-# # endpoint = os.getenv("AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT")
-# # key = os.getenv("AZURE_DOCUMENT_INTELLIGENCE_KEY")
-
-# # document_analysis_client = DocumentAnalysisClient(
-# #     endpoint=endpoint, credential=AzureKeyCredential(key)
-# # )
-
-# with open(os.path.join('ldv', '20231107 131436', "distinta-00.pdf"),'rb') as f:
-#     poller = document_analysis_client.begin_analyze_document("prebuilt-layout", document=f)
-# result = poller.result()
-
-
-
-#end test
-
-
-if username == '':
-    st.session_state["authentication_status"] = True
-    st.session_state["name"] = "John Smith"
-
-if st.session_state["authentication_status"]:
-    st.write(f'Welcome *{st.session_state["name"]}*, you are an **admin**.')
-    
-    import lib.common as common
-    common.clean_session()
-    
-elif st.session_state["authentication_status"] == False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] == None:
-    st.warning('Please enter your username and password')
+if st.session_state['authentication_status']:
+    st.write(f'Welcome, you are logged in as {st.session_state["username"]}!')
